3b440229d71296f22aa8f0cef3e09d96b8380c7d
Tim Janney <tim.s.janney@gmail.com>
2015-06-04 00:47:36 +0000

    added destroy method to users controller to cancel membership and stripe subscription

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find( params[:id] )
   end
+  
+  def destroy
+    @user = current_user
+    customer = Stripe::Customer.retrieve(@user.stripe_customer_token)
+    customer.cancel_subscription()
+    if @user.destroy
+      flash.alert = "Your subscription and account has been cancelled successfully!"
+      redirect_to root_path
+    end
+  end
 end
